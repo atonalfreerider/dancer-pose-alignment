94344fc5250dc5411cf878a772bfddee9ba534da
john <john@primitive.io>
2023-11-11 14:18:48 -0800

    strip wip

--- SqliteOutput.cs
+++ SqliteOutput.cs
@@ -10,14 +10,15 @@ namespace dancer_pose_alignment;
 public class SqliteOutput
 {
     readonly string DbPath;
-    readonly int TotalFrames;
 
-    public SqliteOutput(string dbPath, int totalFrames)
+    public SqliteOutput(string dbPath)
     {
         DbPath = dbPath;
-        TotalFrames = totalFrames;
 
-        if (File.Exists(DbPath)) return;
+        if (File.Exists(DbPath))
+        {
+            File.Delete(DbPath);
+        }
 
         string cs = $"URI=file:{DbPath}";
         using SQLiteConnection conn = new(cs);
@@ -25,50 +26,10 @@ public class SqliteOutput
         CreateTables(conn);
     }
 
-    public void Serialize(List<Tuple<Dancer, Dancer>> leadAndFollowPoses)
-    {
-        string cs = $"URI=file:{DbPath}";
-        using SQLiteConnection conn = new(cs);
-        conn.Open();
-
-        if (leadAndFollowPoses.Count == 1)
-        {
-            InsertDancer(conn, leadAndFollowPoses[0].Item1);
-            InsertDancer(conn, leadAndFollowPoses[0].Item2);
-        }
-        else
-        {
-            Cache(conn, leadAndFollowPoses);
-        }
-    }
 
     static void CreateTables(SQLiteConnection conn)
     {
         using IDbCommand cmd = conn.CreateCommand();
-        cmd.CommandText =
-            """
-            CREATE TABLE lead (
-             id INTEGER PRIMARY KEY ASC,
-             frame_id INTEGER NOT NULL,
-             position_x REAL NOT NULL,
-             position_y REAL NOT NULL,
-             position_z REAL NOT NULL
-            )
-            """;
-        cmd.ExecuteNonQuery();
-
-        cmd.CommandText =
-            """
-            CREATE TABLE follow (
-               id INTEGER PRIMARY KEY ASC,
-               frame_id INTEGER NOT NULL,
-               position_x REAL NOT NULL,
-               position_y REAL NOT NULL,
-               position_z REAL NOT NULL
-             )
-            """;
-        cmd.ExecuteNonQuery();
-
         cmd.CommandText =
             """
             CREATE TABLE cache (
@@ -83,10 +44,12 @@ public class SqliteOutput
         cmd.ExecuteNonQuery();
     }
 
-    static void Cache(
-        IDbConnection conn,
-        List<Tuple<Dancer, Dancer>> dancersByCamera)
+    public void Serialize(List<List<List<List<Vector2>>>> allCameras)
     {
+        string cs = $"URI=file:{DbPath}";
+        using SQLiteConnection conn = new(cs);
+        conn.Open();
+        
         using IDbCommand cmd = conn.CreateCommand();
         using IDbTransaction transaction = conn.BeginTransaction();
         cmd.CommandText =
@@ -108,177 +71,62 @@ public class SqliteOutput
             """;
 
         int camCount = 0;
-        foreach (Tuple<Dancer, Dancer> leadAndFollow in dancersByCamera)
+        foreach (List<List<List<Vector2>>> camera in allCameras)
         {
-            int leadFrameCount = 0;
-            foreach (List<Vector3> pose in leadAndFollow.Item1.PosesByFrame)
+            int frameCount = 0;
+            foreach (List<List<Vector2>> frame in camera)
             {
-                foreach (Vector3 position in pose)
+                int dancerIdCount = 0;
+                foreach (List<Vector2> pose in frame)
                 {
-                    IDbDataParameter dancerIdParameter =
-                        cmd.CreateParameter();
-                    dancerIdParameter.DbType = DbType.Int32;
-                    dancerIdParameter.ParameterName = "@DancerId";
-                    dancerIdParameter.Value = 0;
-                    cmd.Parameters.Add(dancerIdParameter);
+                    foreach (Vector2 position in pose)
+                    {
+                        IDbDataParameter dancerIdParameter =
+                            cmd.CreateParameter();
+                        dancerIdParameter.DbType = DbType.Int32;
+                        dancerIdParameter.ParameterName = "@DancerId";
+                        dancerIdParameter.Value = dancerIdCount;
+                        cmd.Parameters.Add(dancerIdParameter);
 
-                    IDbDataParameter cameraIdParameter =
-                        cmd.CreateParameter();
-                    cameraIdParameter.DbType = DbType.Int32;
-                    cameraIdParameter.ParameterName = "@CameraId";
-                    cameraIdParameter.Value = camCount;
-                    cmd.Parameters.Add(cameraIdParameter);
+                        IDbDataParameter cameraIdParameter =
+                            cmd.CreateParameter();
+                        cameraIdParameter.DbType = DbType.Int32;
+                        cameraIdParameter.ParameterName = "@CameraId";
+                        cameraIdParameter.Value = camCount;
+                        cmd.Parameters.Add(cameraIdParameter);
 
-                    IDbDataParameter frameIdParameter =
-                        cmd.CreateParameter();
-                    frameIdParameter.DbType = DbType.Int32;
-                    frameIdParameter.ParameterName = "@FrameId";
-                    frameIdParameter.Value = leadFrameCount;
-                    cmd.Parameters.Add(frameIdParameter);
+                        IDbDataParameter frameIdParameter =
+                            cmd.CreateParameter();
+                        frameIdParameter.DbType = DbType.Int32;
+                        frameIdParameter.ParameterName = "@FrameId";
+                        frameIdParameter.Value = frameCount;
+                        cmd.Parameters.Add(frameIdParameter);
 
-                    IDbDataParameter positionXParameter =
-                        cmd.CreateParameter();
-                    positionXParameter.DbType = DbType.Double;
-                    positionXParameter.ParameterName = "@PositionX";
-                    positionXParameter.Value = position.X;
-                    cmd.Parameters.Add(positionXParameter);
+                        IDbDataParameter positionXParameter =
+                            cmd.CreateParameter();
+                        positionXParameter.DbType = DbType.Double;
+                        positionXParameter.ParameterName = "@PositionX";
+                        positionXParameter.Value = position.X;
+                        cmd.Parameters.Add(positionXParameter);
 
-                    IDbDataParameter positionYParameter =
-                        cmd.CreateParameter();
-                    positionYParameter.DbType = DbType.Double;
-                    positionYParameter.ParameterName = "@PositionY";
-                    positionYParameter.Value = position.Y;
-                    cmd.Parameters.Add(positionYParameter);
+                        IDbDataParameter positionYParameter =
+                            cmd.CreateParameter();
+                        positionYParameter.DbType = DbType.Double;
+                        positionYParameter.ParameterName = "@PositionY";
+                        positionYParameter.Value = position.Y;
+                        cmd.Parameters.Add(positionYParameter);
 
-                    IDbDataParameter positionZParameter =
-                        cmd.CreateParameter();
-                    positionZParameter.DbType = DbType.Double;
-                    positionZParameter.ParameterName = "@PositionZ";
-                    positionZParameter.Value = position.Z;
-                    cmd.Parameters.Add(positionZParameter);
-
-                    cmd.ExecuteNonQuery();
-                    
+                        cmd.ExecuteNonQuery();
+                        
+                     
+                    }
+                    dancerIdCount++;
                 }
-                leadFrameCount++;
-            }
 
-            int followFrameCount = 0;
-            foreach (List<Vector3> pose in leadAndFollow.Item2.PosesByFrame)
-            {
-                foreach (Vector3 position in pose)
-                {
-                    IDbDataParameter dancerIdParameter =
-                        cmd.CreateParameter();
-                    dancerIdParameter.DbType = DbType.Int32;
-                    dancerIdParameter.ParameterName = "@DancerId";
-                    dancerIdParameter.Value = 1;
-                    cmd.Parameters.Add(dancerIdParameter);
-
-                    IDbDataParameter cameraIdParameter =
-                        cmd.CreateParameter();
-                    cameraIdParameter.DbType = DbType.Int32;
-                    cameraIdParameter.ParameterName = "@CameraId";
-                    cameraIdParameter.Value = camCount;
-                    cmd.Parameters.Add(cameraIdParameter);
-
-                    IDbDataParameter frameIdParameter =
-                        cmd.CreateParameter();
-                    frameIdParameter.DbType = DbType.Int32;
-                    frameIdParameter.ParameterName = "@FrameId";
-                    frameIdParameter.Value = followFrameCount;
-                    cmd.Parameters.Add(frameIdParameter);
-
-                    IDbDataParameter positionXParameter =
-                        cmd.CreateParameter();
-                    positionXParameter.DbType = DbType.Double;
-                    positionXParameter.ParameterName = "@PositionX";
-                    positionXParameter.Value = position.X;
-                    cmd.Parameters.Add(positionXParameter);
-
-                    IDbDataParameter positionYParameter =
-                        cmd.CreateParameter();
-                    positionYParameter.DbType = DbType.Double;
-                    positionYParameter.ParameterName = "@PositionY";
-                    positionYParameter.Value = position.Y;
-                    cmd.Parameters.Add(positionYParameter);
-
-                    IDbDataParameter positionZParameter =
-                        cmd.CreateParameter();
-                    positionZParameter.DbType = DbType.Double;
-                    positionZParameter.ParameterName = "@PositionZ";
-                    positionZParameter.Value = position.Z;
-                    cmd.Parameters.Add(positionZParameter);
-
-                    cmd.ExecuteNonQuery();
-                }
-                followFrameCount++;
+                frameCount++;
             }
 
             camCount++;
-        }
-
-        transaction.Commit();
-    }
-
-    void InsertDancer(
-        IDbConnection conn,
-        Dancer dancer)
-    {
-        using IDbCommand cmd = conn.CreateCommand();
-        using IDbTransaction transaction = conn.BeginTransaction();
-        cmd.CommandText =
-            @"INSERT INTO" + (dancer.Role == Role.Lead ? " lead " : " follow ") + """
-                
-                                      (
-                                          frame_id,
-                                          position_x,
-                                          position_y,
-                                          position_z
-                                      ) VALUES (
-                                          @FrameId,
-                                          @PositionX,
-                                          @PositionY,
-                                          @PositionZ
-                                      )
-                """;
-
-        for (int i = 0; i < TotalFrames; i++)
-        {
-            if (dancer.PosesByFrame.Count <= i) break;
-            List<Vector3> pose = dancer.PosesByFrame[i];
-            foreach (Vector3 position in pose)
-            {
-                IDbDataParameter frameIdParameter =
-                    cmd.CreateParameter();
-                frameIdParameter.DbType = DbType.Int32;
-                frameIdParameter.ParameterName = "@FrameId";
-                frameIdParameter.Value = i;
-                cmd.Parameters.Add(frameIdParameter);
-
-                IDbDataParameter positionXParameter =
-                    cmd.CreateParameter();
-                positionXParameter.DbType = DbType.Double;
-                positionXParameter.ParameterName = "@PositionX";
-                positionXParameter.Value = position.X;
-                cmd.Parameters.Add(positionXParameter);
-
-                IDbDataParameter positionYParameter =
-                    cmd.CreateParameter();
-                positionYParameter.DbType = DbType.Double;
-                positionYParameter.ParameterName = "@PositionY";
-                positionYParameter.Value = position.Y;
-                cmd.Parameters.Add(positionYParameter);
-
-                IDbDataParameter positionZParameter =
-                    cmd.CreateParameter();
-                positionZParameter.DbType = DbType.Double;
-                positionZParameter.ParameterName = "@PositionZ";
-                positionZParameter.Value = position.Z;
-                cmd.Parameters.Add(positionZParameter);
-
-                cmd.ExecuteNonQuery();
-            }
         }
 
         transaction.Commit();
